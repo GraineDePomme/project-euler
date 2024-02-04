191d8f3cd2ae9a26f09c2c4bd185173b8e7e5e2b
Sarisson Guillaume <Guillaume.Sarisson@gmail.com>
2024-02-04 11:54:27 +0100

    Add files via upload

---
+++ Problem_0001.hs
@@ -0,0 +1,18 @@
+-- Problem #1 of project Euler :
+
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5,
+-- we get 3, 5, 6, and 9.
+-- The sum of these multiples is 23.
+
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+multiplesOf3 = [3, 6..1000] :: [Int]
+
+multiplesOf5 = [5, 10..1000] :: [Int]
+
+multiplesOf15 = [15, 30..1000] :: [Int]
+
+
+finalResult :: Int
+finalResult = sum multiplesOf3 + sum multiplesOf5 - sum multiplesOf15 - 1000
