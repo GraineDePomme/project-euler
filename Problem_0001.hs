89dc6b135b483ba7b2672691284bb3413e7a82da
Sarisson Guillaume <Guillaume.Sarisson@gmail.com>
2024-02-04 12:34:35 +0100

    Update Problem_0001.hs

--- Problem_0001.hs
+++ Problem_0001.hs
@@ -1,4 +1,4 @@
--- Problem #1 of project Euler :
+-- Problem #1 of project Euler : Multiples of 3 or 5
 
 -- If we list all the natural numbers below 10 that are multiples of 3 or 5,
 -- we get 3, 5, 6, and 9.
